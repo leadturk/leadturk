bb72e41c0d4dda23aedf9a3122bd66c2aae25b6a
Jesse <jesse@localhost.localdomain>
2020-03-13 11:51:17 +0700

    cc

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
-	console.log("ready");
 	var mHeight = $(document).height();
     $(window).scroll(function(){
         var sPosition = $(window).scrollTop();
-        console.log(sPosition);
         if(sPosition > (mHeight/2)) {
         	$(".sticky-share").addClass('visible');
         }
